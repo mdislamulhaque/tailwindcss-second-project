50bb03d6eace02bf4d18b4a0a2061c794f04da9a
Md.Islamul Haque <md.islamul@henkansolutions.com>
2024-09-10 16:16:05 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: { 
+        Poppins: ['poppins', 'sans-serif'],
+      },
+      colors: {
+        'bg-color':'#091420',
+        'bg-dark-color':'#0c1a28',
+        'primary-color':'#4fe98c',
+    },
+
+    width: {
+        'custom-image-width': '200px',
+    },
+    },
+ 
+  },
+  plugins: [],
+}
+
